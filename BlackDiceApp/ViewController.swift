fbbf7f999dbfac9161fd885ad649fb3269e2bbb5
Dilara Elçioğlu <delcioglu@gmail.com>
2022-08-20 13:33:25 +0300

    View'a gestureRecognizer ekledim ekranı tıklanabilir yaptım. Shake eklemedim henüz bilmiyorum yapılışını Bi de dark moda aldığımda zarlar siyah olduğundan kayboluyo onu çözmem gerek Şimdilik bu kadar hocam saygılar

--- BlackDiceApp/ViewController.swift
+++ BlackDiceApp/ViewController.swift
@@ -13,13 +13,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImage2: UIImageView!
     
+    var dice1 = UIImage(imageLiteralResourceName: "dice1")
+    var dice2 = UIImage(imageLiteralResourceName: "dice2")
+    var dice3 = UIImage(imageLiteralResourceName: "dice2")
+    var dice4 = UIImage(imageLiteralResourceName: "dice4")
+    var dice5 = UIImage(imageLiteralResourceName: "dice5")
+    var dice6 = UIImage(imageLiteralResourceName: "dice6")
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-       
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scrollDice))
+        view.addGestureRecognizer(gestureRecognizer)
     }
+    
+    @objc func scrollDice() {
+        let diceArray = [dice1,dice2,dice3,dice4,dice5,dice6,]
 
+        diceImage1.image = diceArray [Int.random(in: 0...5)]
+        diceImage2.image = diceArray [Int.random(in: 0...5)]
+
+        
+    }
 
 }
 
