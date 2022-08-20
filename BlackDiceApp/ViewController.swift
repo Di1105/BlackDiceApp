5af5067f78866f3bc387062ae8025d0b4fdb39c6
Dilara Elçioğlu <delcioglu@gmail.com>
2022-08-20 12:11:31 +0300

    First Commit

--- BlackDiceApp/ViewController.swift
+++ BlackDiceApp/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImage1: UIImageView!
+    
+    @IBOutlet weak var diceImage2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
     }
 
 
